ff7313c39e801cc4cebb39206fbb31debd7e8d75
Daniel Kolkena <daniel.kolkena@taulia.com>
2015-05-09 11:20:52 -0600

    Draws a square

--- hellodrone.py
+++ hellodrone.py
@@ -2,9 +2,32 @@
 
 import time
 from turtledrone import TRDrone 
-drone = TRDrone() 
-drone.takeoff()
-drone.write("{} says hi to {}".format('Daniel', drone.name))
-time.sleep(5)
-drone.land()
-drone.halt()
+
+drony = TRDrone()
+
+def draw_square(drone):
+	""" Draws a square with a drone
+	"""
+	d = drone
+	d.takeoff()
+	for i in range(10):
+		d.move_forward()
+	for i in range(18):
+		d.turn_left()
+	for i in range(10):
+		d.move_forward()
+	for i in range(18):
+		d.turn_left()
+	for i in range(10):
+		d.move_forward()
+	for i in range(18):
+		d.turn_left()
+	for i in range(10):
+		d.move_forward()
+	for i in range(18):
+		d.turn_left()
+	d.land()
+	time.sleep(5)
+	d.halt()
+
+draw_square(drony)
